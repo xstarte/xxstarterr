9e977512f9b26fb67c9a038f0fb9f551cc85ca81
xstarte <ienotovstar@gmail.com>
2024-10-09 01:33:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const telegram = window.Telegram.WebApp;
+
+document.getElementById('someButton').onclick = () => {
+    alert("Кнопка нажата!");
+    // Здесь можно добавить логику для взаимодействия с ботом
+};
